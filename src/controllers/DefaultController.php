9c46ea4bb5ee9761b79a13ebf9d531fd47eeab6e
brandonkelly <brandon@pixelandtonic.com>
2026-02-17 17:52:50 -0800

    $defaultAction

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -16,14 +16,7 @@ use yii\web\Response;
  */
 class DefaultController extends BaseController
 {
-    /**
-     * @inheritdoc
-     */
-    public function init(): void
-    {
-        parent::init();
-        $this->defaultAction = 'load-bucket-data';
-    }
+    public $defaultAction = 'load-bucket-data';
 
     /**
      * Load bucket data for specified credentials.
